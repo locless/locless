1bfa559b496bbad1e09c1f1c15bb88eb7f7ac5b4
twendykirn <xan50rus@gmail.com>
2024-03-14 16:53:51 +0100

    feat: add expo packages

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient, useQuery } from "convex/react";
+import { api } from "@repo/backend/convex/_generated/api";
+import "react-native-get-random-values";
+import {
+  BarcodeScanningResult,
+  CameraView,
+  PermissionStatus,
+  useCameraPermissions,
+} from "expo-camera/next";
+import { useEffect, useState } from "react";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL, {
+  unsavedChangesWarning: false,
+});
 
 export default function App() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const [didScan, setDidScan] = useState(false);
+
+  const handleBarcodeScanned = (scanningResult: BarcodeScanningResult) => {
+    console.log(scanningResult.data);
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Welcome to Locless</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <View style={styles.container}>
+          <CameraView
+            barcodeScannerSettings={{
+              barcodeTypes: ["qr"],
+            }}
+            facing="back"
+            style={{flex: 1,width:"100%"}}
+            onBarcodeScanned={handleBarcodeScanned}
+          />
+        </View>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
