6f8d8a4691a7f78322395ab9efcd8c894e48cce1
twendykirn <xan50rus@gmail.com>
2024-04-22 19:28:54 +0200

    feat: add small locless cli

---
+++ packages/locless/src/commands/deploy.ts
@@ -0,0 +1,55 @@
+import { Command } from 'commander';
+import ora from 'ora';
+import path from 'path';
+import * as fs from 'fs';
+import child_process from 'child_process';
+
+const spinner = ora({
+    text: 'Loading...',
+    color: 'yellow',
+});
+
+export const deploy = new Command()
+    .name('deploy')
+    .description('Transform and deploy TS file to Locless cloud.')
+    .action(async () => {
+        await runExample();
+    });
+
+export async function runExample() {
+    spinner.start('Searching for locless folder...');
+
+    const __dirname = path.resolve();
+
+    const loclessFolderPath = path.join(__dirname, 'locless');
+
+    if (!fs.existsSync(loclessFolderPath)) {
+        spinner.fail(`Locless folder not found in ${loclessFolderPath}`);
+        return;
+    }
+
+    spinner.text = 'Locless folder found...';
+
+    await fs.readdir(loclessFolderPath, (err, files) => {
+        spinner.text = 'Scanning Locless folder...';
+        files.forEach(file => {
+            if (file.includes('.jsx')) {
+                spinner.text = `Found ${file}...`;
+
+                const filePath = path.join(__dirname, 'locless', file);
+
+                const buildPath = path.join(__dirname, 'locless', 'build', file.replace('.jsx', '.js'));
+
+                try {
+                    child_process.execSync(`npx babel ${filePath} -o ${buildPath}`).toString();
+                    spinner.succeed(`Compiled ${file} to ${buildPath}`);
+                } catch (e) {
+                    spinner.fail(`${e}`);
+                    return;
+                }
+            }
+        });
+    });
+
+    return `Uploaded to Locless cloud`;
+}
