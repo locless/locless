7ea0110294b9bbb90905f85d515b580f4929d659
twendykirn <xan50rus@gmail.com>
2024-06-10 19:56:31 +0200

    refactor: remove styles

--- apps/web/app/(authenticated)/auth/sign-in/oauth-signin.tsx
+++ apps/web/app/(authenticated)/auth/sign-in/oauth-signin.tsx
@@ -7,54 +7,44 @@ import { useSignIn } from '@clerk/nextjs';
 import * as React from 'react';
 
 export function OAuthSignIn() {
-    const [isLoading, setIsLoading] = React.useState<any>(null);
-    const { signIn, isLoaded: signInLoaded } = useSignIn();
+  const [isLoading, setIsLoading] = React.useState<any>(null);
+  const { signIn, isLoaded: signInLoaded } = useSignIn();
 
-    const { toast } = useToast();
+  const { toast } = useToast();
 
-    const oauthSignIn = async (provider: any) => {
-        if (!signInLoaded) {
-            return null;
-        }
-        try {
-            setIsLoading(provider);
-            await signIn.authenticateWithRedirect({
-                strategy: provider,
-                redirectUrl: '/auth/sso-callback',
-                redirectUrlComplete: '/app/projects',
-            });
-        } catch (err) {
-            console.error(err);
-            setIsLoading(null);
-            toast({
-                variant: 'destructive',
-                description: (err as Error).message,
-            });
-        }
-    };
+  const oauthSignIn = async (provider: any) => {
+    if (!signInLoaded) {
+      return null;
+    }
+    try {
+      setIsLoading(provider);
+      await signIn.authenticateWithRedirect({
+        strategy: provider,
+        redirectUrl: '/auth/sso-callback',
+        redirectUrlComplete: '/app/projects',
+      });
+    } catch (err) {
+      console.error(err);
+      setIsLoading(null);
+      toast({
+        variant: 'destructive',
+        description: (err as Error).message,
+      });
+    }
+  };
 
-    return (
-        <div className='flex flex-col gap-2'>
-            <Button
-                className='bg-gray-50 text-black border-gray-300 hover:bg-black hover:text-white'
-                onClick={() => oauthSignIn('oauth_github')}>
-                {isLoading === 'oauth_github' ? (
-                    <Loading className='w-4 h-4 mr-2' />
-                ) : (
-                    <Icons.gitHub className='w-4 h-4 mr-2' />
-                )}
-                GitHub
-            </Button>
-            <Button
-                className='bg-gray-50 text-black border-gray-300 hover:bg-black hover:text-white'
-                onClick={() => oauthSignIn('oauth_google')}>
-                {isLoading === 'oauth_google' ? (
-                    <Loading className='w-4 h-4 mr-2' />
-                ) : (
-                    <Icons.google className='w-4 h-4 mr-2' />
-                )}
-                Google
-            </Button>
-        </div>
-    );
+  return (
+    <div className='flex flex-col gap-2'>
+      <Button
+        className='bg-gray-50 text-black border-gray-300 hover:bg-black hover:text-white'
+        onClick={() => oauthSignIn('oauth_github')}>
+        {isLoading === 'oauth_github' ? (
+          <Loading className='w-4 h-4 mr-2' />
+        ) : (
+          <Icons.gitHub className='w-4 h-4 mr-2' />
+        )}
+        GitHub
+      </Button>
+    </div>
+  );
 }
