1716e4d0bc9da6b5cd49a12acd5d13f36898618f
twendykirn <xan50rus@gmail.com>
2024-08-25 07:19:12 +0200

    feat: update discord invite link

--- apps/web/app/(landing)/discord/page.tsx
+++ apps/web/app/(landing)/discord/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from 'next/navigation';
 
-// TODO: add discord link
 export default function DiscordRedirect() {
-  return redirect('/');
+  return redirect('https://discord.gg/jdKu4Pdpw6');
 }
